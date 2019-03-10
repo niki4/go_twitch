9d25edfc6e748a9fbc9b7eb6d50f51fffd5dcbd7
Ivan Nikiforov <nikiforr@gmail.com>
2019-03-10 20:00:20 +0300

    Add user login form and skeleton code for related handlers

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -1,15 +1,22 @@
 package api
 
-import "github.com/qiangxue/fasthttp-routing"
+import (
+	"fmt"
+	"github.com/qiangxue/fasthttp-routing"
+	"github.com/valyala/fasthttp"
+)
 
 // ShowLoginPage dispatch request for Login Page
 func (r *Router) ShowLoginPage(ctx *routing.Context) error {
-
+	fasthttp.ServeFile(ctx.RequestCtx, "./static/login_form.html")
 	return nil
 }
 
 // DoLogin dispatch request to perform Login action
 func (r *Router) DoLogin(ctx *routing.Context) error {
+	login := ctx.FormValue("username")
+	pass := ctx.FormValue("password")
 
+	fmt.Fprintf(ctx, "You logged in :) \nLogin: %s\nPassword: %s", login, pass)
 	return nil
 }
