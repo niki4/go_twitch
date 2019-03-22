a345235cceaf242050f07a976a7cbaaa26d9c97a
Ivan Nikiforov <nikiforr@gmail.com>
2019-03-22 15:15:37 +0300

    Fix redirect URI

--- cmd/api/auth.go
+++ cmd/api/auth.go
@@ -58,7 +58,7 @@ func (r *Router) ShowLoginPage(ctx *routing.Context) error {
 		URL: URLData{
 			Base:         "https://id.twitch.tv/oauth2/authorize",
 			ClientID:     "zhhxr55p8a8ft88s88mp0nng3ssqhd",
-			RedirectURI:  fmt.Sprintf("http://%s:%s", r.Host, r.Port),
+			RedirectURI:  fmt.Sprintf("http://%s:%s", r.Host, "80"),
 			ResponseType: "code",
 			Scope:        "viewing_activity_read",
 		},
